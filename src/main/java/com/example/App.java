b97633e5b2671ed4b7d79a6047c7f32910fed8f0
Ian Morse <ian@screenscape.com>
2017-07-25 11:35:27 -0300

    changing version to 1.1.0

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@
 package com.example;
 
 public class App {
-    private static final String VERSION_INFO = "V 1.0.2 master";
+    private static final String VERSION_INFO = "V 1.1.0 master";
 
     public static void main(String[] args) {
         System.out.println("TRACER hola from app. version: " + VERSION_INFO);
